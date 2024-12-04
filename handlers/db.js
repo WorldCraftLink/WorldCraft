3944d4ec864ebfa73bcbb26e0d50a0594be5b070
WorldCraftLink <itsgamerluffy@gmail.com>
2024-12-04 18:51:15 +0800

    Update db.js

--- handlers/db.js
+++ handlers/db.js
@@ -8,14 +8,14 @@ let db;
 
 if (config.databaseURL.startsWith('sqlite')) {
     const Keyv = require('keyv');
-    db = new Keyv('sqlite://world.db');
+    db = new Keyv('sqlite://skyport.db');
 } else {
     const Keyv = require('@keyvhq/core');
     const KeyvMysql = require('@keyvhq/mysql');
 
     const mysqlConfig = {
         url: config.databaseURL,
-        table: 'world',
+        table: 'skyport',
         keySize: 255,
     };
 
